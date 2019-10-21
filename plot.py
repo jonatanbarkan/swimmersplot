3e0c40e4ff9bc7e6717591c99cf1e70dd5dc6861
Jonatan Barkan <jonatan@senswim.com>
2019-10-21 21:33:30 +0300

    fixed some of overlapping lines but some remain

--- plot.py
+++ plot.py
@@ -4,12 +4,38 @@ import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt, patches as mpatches
 from matplotlib import style
+from matplotlib.collections import LineCollection
 
-from utils import split_spines, adjust_legend, read_csv, label_bin_values, combine
-from argparse import ArgumentParser
+from utils import split_spines, adjust_legend
+
 style.use('seaborn-paper')
-colors = ['r', 'b', 'g', 'c', 'o']
-markers = [">", "*", "s", '8', "P", "1"]
+
+color_dict = dict(under='r', drop='y', both='darkorange', normal='b')
+base_name = 'EF'
+under = 50
+drop = 10
+color_label_dict = dict(
+    under='{} < {}'.format(base_name, under),
+    drop='{} < Decline'.format(drop),
+    both='{} < {}'.format(base_name, under) + ', ' + '{} < Decline'.format(drop),
+    normal='Normal {}'.format(base_name))
+marker_dict = {
+    'cessation of treatment': "s",
+    'disease progression, resume treatment': "$>>$",
+    'resume treatment': ">",
+    'temporary cessation': "$||$",
+}
+
+
+def select_color(d, u):
+    if d and u:
+        return color_dict['both']
+    if d:
+        return color_dict['under']
+    if u:
+        return color_dict['drop']
+    return color_dict['normal']
+
 
 if __name__ == '__main__':
     dfs = pd.read_excel('demo.xlsx', sheet_name=None, header=None, )
@@ -17,68 +43,68 @@ if __name__ == '__main__':
     months = dfs['months']
     values = dfs['values']
     types = dfs['types'].fillna('')
-    # months_path = 'months_demo.csv'
-    # months = read_csv(months_path, int)
-    # values_path = 'values_demo.csv'
-    # values = read_csv(values_path, int)
-    # types_path = 'types_demo.csv'
-    # types = read_csv(types_path, str)
 
-    base_name = 'EF'
-    value_bins = [50]
-    # kinds = ['a', 'b', 'c', 'dd']
-    value_names = label_bin_values(value_bins, base_name)
-    Point = namedtuple('Point', "patient month value binned kind")
+    Point = namedtuple('Point', 'x y kind')
     points = []
+    lines = []
+    colors = []
     kinds = set()
     M = 0
     for i, (m, v, t) in enumerate(zip(months.values, values.values, types.values)):
-        print('patient {}'.format(i, ))
+        current_patient = i + 1
         m = m[np.isfinite(m)]
         v = v[np.isfinite(v)]
         length = len(m)
+        current_month = m[0]
+        last_month = m[-1]
+        first_value = v[0]
+        v_drop = first_value - v > drop
+        v_under = v < under
+        current_drop = v_drop[0]
+        current_under = v_under[0]
+        current_color = select_color(current_drop, current_under)
         if length > M:
             M = length
-        binned_v = np.digitize(v, value_bins)
         for j in range(length):
+            # add point for markers
             try:
-                tp = t[j]
+                k = t[j].strip()
+                if k:
+                    points.append(Point(m[j], current_patient, k))
+                    kinds.add(k)
             except IndexError:
-                tp = ""
-            kinds.add(tp)
-            points.append(Point(i + 1, m[j], v[j], binned_v[j], tp))
-    kinds.remove("")
+                pass
+            dr = current_drop != v_drop[j]
+            un = current_under != v_under[j]
+            if dr or un:
+                lines.append([(current_month, current_patient), (m[j], current_patient)])
+                colors.append(current_color)
+                current_month = m[j]
+                current_under = v_under[j]
+                current_drop = v_drop[j]
+                current_color = select_color(dr, un)
+        if current_month != last_month:
+            lines.append([(current_month, current_patient), (last_month, current_patient)])
+            colors.append(current_color)
+
     kinds = list(kinds)
 
-    fig = plt.figure()
+    fig = plt.figure(figsize=(20, 10))
     ax = fig.add_subplot(111)
-    count_patients = 0
-    first = 0
-    last_marker = -1
-    for p1, p2 in zip(points[:-1], points[1:]):
-        if p1.binned != p2.binned or p2.kind:
-            if p2.kind:
-                ax.plot(p2.month, p2.patient, marker=markers[kinds.index(p2.kind)], c='k', markeredgewidth=1, markeredgecolor='k')
-                last_marker = p2.month
-            if p1.patient == p2.patient:
-                # ax.plot([first, p2.month], [p1.patient, p1.patient], c=colors[p1.binned], lw=10, alpha=0.3, solid_capstyle='projecting', solid_joinstyle='miter')
-                ax.plot([first, p2.month], [p1.patient, p1.patient], c=colors[p1.binned], lw=10, alpha=0.3, solid_capstyle='projecting')
-            first = p2.month + 1
-        if p1.patient != p2.patient:
-            count_patients += 1
-            # ax.plot([first, max(last_marker, p1.month)], [p1.patient, p1.patient], c=colors[p1.binned], lw=10, alpha=0.3, solid_capstyle='projecting', solid_joinstyle='miter')
-            ax.plot([first, max(last_marker, p1.month)], [p1.patient, p1.patient], c=colors[p1.binned], lw=10, alpha=0.3, solid_capstyle='projecting')
-            first = 0
-            last_marker = -1
-
+    alpha = 0.5
+    lc = LineCollection(lines, colors=colors, linewidths=10)
+    lc.set_alpha(alpha)
+    ax.add_collection(lc)
+    # ax.autoscale()
     split_spines(ax)
     ax.set_xlabel('Months')
     ax.set_ylabel('Patients')
-
-    values_handle = [mpatches.Patch(color=c, label=l, alpha=0.3) for (c, l) in zip(colors, value_names)]
-    marker_handle = [plt.plot([], [], marker=mark, ms=10, ls="", mec=None, color='k', label=kind)[0] for mark, kind in zip(markers, kinds)]
-    lg = plt.legend(handles=values_handle + marker_handle)
+    for p in points:
+        ax.plot(p.x, p.y, marker=marker_dict[p.kind], c='k', markeredgewidth=1, markeredgecolor='k')
+    values_handle = [mpatches.Patch(color=c, label=color_label_dict[l], alpha=alpha) for (l, c) in color_dict.items()]
+    marker_handle = [plt.plot([], [], marker=mark, ms=10, ls="", mec=None, color='k', label=kind, markeredgewidth=1, markeredgecolor='k')[0] for kind, mark in marker_dict.items()]
+    lg = plt.legend(handles=values_handle + marker_handle, loc='lower right')
     adjust_legend(lg)
     ax.add_artist(lg)
-    plt.pause(1)
-    a = 0
+    # plt.pause(1)
+    plt.show()
