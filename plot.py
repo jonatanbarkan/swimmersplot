0eb2bfb2f5a14cf320b51e31d00bc0f7bd8c6e69
Jonatan Barkan <jonatan@senswim.com>
2019-10-22 21:51:42 +0300

    dirty but working

--- plot.py
+++ plot.py
@@ -30,10 +30,10 @@ marker_dict = {
 def select_color(d, u):
     if d and u:
         return color_dict['both']
-    if d:
-        return color_dict['under']
-    if u:
+    elif d:
         return color_dict['drop']
+    elif u:
+        return color_dict['under']
     return color_dict['normal']
 
 
@@ -48,18 +48,23 @@ if __name__ == '__main__':
     points = []
     lines = []
     colors = []
+    linestyles = []
     kinds = set()
     M = 0
     for i, (m, v, t) in enumerate(zip(months.values, values.values, types.values)):
         current_patient = i + 1
         m = m[np.isfinite(m)]
+        m_diff = np.diff(m)
+        m_diff = np.insert(m_diff, 0, 9)
+        m += (m_diff == 0) * np.full(len(m), .5)  # take month with 2 values into account
         v = v[np.isfinite(v)]
         length = len(m)
         current_month = m[0]
         last_month = m[-1]
         first_value = v[0]
-        v_drop = first_value - v > drop
+        v_drop = (first_value - v) >= drop
         v_under = v < under
+        v_both = v_under * v_drop
         current_drop = v_drop[0]
         current_under = v_under[0]
         current_color = select_color(current_drop, current_under)
@@ -79,20 +84,26 @@ if __name__ == '__main__':
             if dr or un:
                 lines.append([(current_month, current_patient), (m[j], current_patient)])
                 colors.append(current_color)
+                linestyles.append('solid')
                 current_month = m[j]
                 current_under = v_under[j]
                 current_drop = v_drop[j]
-                current_color = select_color(dr, un)
+                current_color = select_color(current_drop, current_under)
         if current_month != last_month:
             lines.append([(current_month, current_patient), (last_month, current_patient)])
             colors.append(current_color)
+            linestyles.append('solid')
+        # add last value as dashed line
+        lines.append([(last_month, current_patient), (last_month+6, current_patient)])
+        colors.append(select_color(v_drop[-1], v_under[-1]))
+        linestyles.append('dotted')
 
     kinds = list(kinds)
 
     fig = plt.figure(figsize=(20, 10))
     ax = fig.add_subplot(111)
-    alpha = 0.5
-    lc = LineCollection(lines, colors=colors, linewidths=10)
+    alpha = 0.4
+    lc = LineCollection(lines, colors=colors, linestyles=linestyles, linewidths=10)
     lc.set_alpha(alpha)
     ax.add_collection(lc)
     # ax.autoscale()
@@ -102,9 +113,11 @@ if __name__ == '__main__':
     for p in points:
         ax.plot(p.x, p.y, marker=marker_dict[p.kind], c='k', markeredgewidth=1, markeredgecolor='k')
     values_handle = [mpatches.Patch(color=c, label=color_label_dict[l], alpha=alpha) for (l, c) in color_dict.items()]
-    marker_handle = [plt.plot([], [], marker=mark, ms=10, ls="", mec=None, color='k', label=kind, markeredgewidth=1, markeredgecolor='k')[0] for kind, mark in marker_dict.items()]
-    lg = plt.legend(handles=values_handle + marker_handle, loc='lower right')
+    marker_handle = [plt.plot([], [], marker=mark, ms=10, ls="", color='k', label=kind, markeredgewidth=1, markeredgecolor='k')[0] for kind, mark in marker_dict.items()]
+    lg = plt.legend(handles=values_handle + marker_handle, loc='best')
     adjust_legend(lg)
     ax.add_artist(lg)
-    # plt.pause(1)
-    plt.show()
+    ax.invert_yaxis()
+    plt.pause(10)
+    # plt.show()
+    fig.savefig('demo.png')
